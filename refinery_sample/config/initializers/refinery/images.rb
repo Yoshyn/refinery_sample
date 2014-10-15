94a0192102b4f9afd9620f79ab88d79cca601280
sylvestre <sylvestre@followanalytics.com>
2014-10-16 00:27:56 +0200

    Fix reloading problem with dragonfly. Set a constant secret.

--- refinery_sample/config/initializers/refinery/images.rb
+++ refinery_sample/config/initializers/refinery/images.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 Refinery::Images.configure do |config|
   # Configures the maximum allowed upload size (in bytes) for an image
-  # config.max_image_size = 5242880
+  config.max_image_size = 5242880
 
   # Configure how many images per page should be displayed when a dialog is presented that contains images
   # config.pages_per_dialog = 18
@@ -17,13 +17,13 @@ Refinery::Images.configure do |config|
   # config.user_image_sizes = {:small=>"110x110>", :medium=>"225x255>", :large=>"450x450>"}
 
   # Configure white-listed mime types for validation
-  # config.whitelisted_mime_types = ["image/jpeg", "image/png", "image/gif", "image/tiff"]
+  config.whitelisted_mime_types = ["image/jpeg", "image/png", "image/gif", "image/tiff"]
 
   # Configure image view options
-  # config.image_views = [:grid, :list]
+  config.image_views = [:grid, :list]
 
   # Configure default image view
-  # config.preferred_image_view = :grid
+  config.preferred_image_view = :grid
 
   # Configure S3 (you can also use ENV for this)
   # The s3_backend setting by default defers to the core setting for this but can be set just for images.
@@ -36,7 +36,7 @@ Refinery::Images.configure do |config|
   # Configure Dragonfly
   # This is where in the middleware stack to insert the Dragonfly middleware
   # config.dragonfly_insert_before = "ActionDispatch::Callbacks"
-  # config.dragonfly_secret = "9d40d0f719975e92cdb254f851fce1fc8841c9f1d3f3d8e1"
+  config.dragonfly_secret = "9d40d0f719975e92cdb254f851fce1fc8841c9f1d3f3d8e1" # Neested cause cause one each reload the the sha1 change
   # config.dragonfly_url_format = "/system/images/:job/:basename.:ext"
   # config.dragonfly_url_host = ""
   # config.datastore_root_path = "/Users/sylvestre/workspaces/samples/refinery_sample/public/system/refinery/images"
